34ae75f8170151a54bdf4d4c9706a2bb5a6e30c7
Baltazar Andersson <baltazardevv@gmail.com>
2023-08-11 23:51:14 -0300

    imprv: fixed bg ball position & initial position

--- src/components/BgBall.svelte
+++ src/components/BgBall.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
+  import { MOBILE_WIDTH } from "@constants/index";
+
+  let innerWidth: number;
+
   const getCoords = () => ({
     left: Math.random() * window.innerWidth,
     top: Math.random() * window.innerHeight,
   });
 
-  let coords = getCoords();
-  let innerWidth: number;
-  let isMobileDevice = false;
+  let coords = { left: window.innerWidth / 2, top: window.innerHeight };
+  let isMobileDevice = window.innerWidth < MOBILE_WIDTH;
+  let divElement: HTMLDivElement;
 
   const updateCoords = ({ x, y }: Record<string, number> = {}) => {
     if (x !== undefined && y !== undefined) {
@@ -16,26 +20,30 @@
     }
   };
 
-  updateCoords();
-
   const handleMouseMove = ({ clientX, clientY }: MouseEvent) => {
     if (isMobileDevice) return;
 
-    updateCoords({ x: clientX, y: clientY });
+    updateCoords({
+      x: clientX + divElement.clientWidth / 2,
+      y: clientY + divElement.clientHeight / 2,
+    });
   };
+
+  !isMobileDevice && updateCoords();
 
   setInterval(() => {
     if (isMobileDevice) updateCoords();
   }, 5000);
 
-  $: isMobileDevice = innerWidth < 1024;
+  $: isMobileDevice = innerWidth < MOBILE_WIDTH;
 </script>
 
 <svelte:window on:mousemove={handleMouseMove} bind:innerWidth />
 
 <div
+  bind:this={divElement}
   style="--left:{coords.left}px; --top:{coords.top}px; transition:{isMobileDevice
-    ? 'left 7s ease-in-out, top 7s ease-in-out'
+    ? 'left 5s ease-in-out, top 5s ease-in-out'
     : ''}"
   class="fixed w-64 h-64 overflow-hidden rounded-full opacity-50 pointer-events-none blur-2xl -z-10 bg-gradient-to-br from-amber-200 via-orange-200 to-red-300 dark:from-amber-300 dark:via-orange-400 dark:to-amber-700 dark:opacity-10"
 />
@@ -43,11 +51,10 @@
 <style>
   @keyframes spin {
     0% {
-      transform: rotate(0deg);
+      transform: translate(-50%, -50%) rotate(0deg);
     }
-
     100% {
-      transform: rotate(360deg);
+      transform: translate(-50%, -50%) rotate(360deg);
     }
   }
 
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -10,3 +10,5 @@ export const DEFAULT_LANGUAGE = "en-EN";
 export const SECTION_CLASS = "landing-section";
 
 export const HEADER_HEIGHT = 80;
+
+export const MOBILE_WIDTH = 1024;
